3f00872b0fe512fbd910c1141c2f26be03749a12
Helieberti Silva <helieberti@hotmail.com>
2026-06-28 09:55:06 -0300

    Mensagem do sistema atualizada

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import { turboLogger } from "./turboLogger.js";
 
 console.log("Usando o github");
 turboLogger("Estou aprendendo o fluxo do git", "red");
-turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema", "darkgreen");
+turboLogger(
+  "Eu consegui criar uma nova mensagem sem afetar a versão principal do sistema",
+  "darkgreen"
+);
